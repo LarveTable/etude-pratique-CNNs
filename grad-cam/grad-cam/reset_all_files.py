c234868e2f1087ad8a33aab42aad9172dd237ffa
LarveTable <cyorick@gmail.com>
2023-11-16 17:00:43 +0100

    Grad-Cam first test commit
    read the README.md to get instructions on how to use the code.

---
+++ grad-cam/grad-cam/reset_all_files.py
@@ -0,0 +1,25 @@
+import os
+
+def delete_files_in_folder(folder_path):
+    # Ensure the folder path is valid
+    if not os.path.exists(folder_path):
+        print(f"The specified folder path '{folder_path}' does not exist.")
+        return
+
+    # Get a list of files in the folder
+    files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+
+    if (files == []):
+        print("No file processed.")
+
+    # Delete each file
+    for file_name in files:
+        file_path = os.path.join(folder_path, file_name)
+        try:
+            os.remove(file_path)
+            print(f"Deleted: {file_name}")
+        except Exception as e:
+            print(f"Error deleting {file_name}: {e}")
+
+delete_files_in_folder('./data/images')
+delete_files_in_folder('./results')
