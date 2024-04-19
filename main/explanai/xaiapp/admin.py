c197f7e8e3c97f69b6ad18efb4f38b0a11607fdc
Sulaymane Dagnet <sulaymane.dagnet@insa-rennes.fr>
2024-04-19 21:09:34 +0200

    webserver setup avec une page main qui lance une expe, les données peuvent être récupérée pour être traitées avec le back-end

--- main/explanai/xaiapp/admin.py
+++ main/explanai/xaiapp/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Config, Image
+from .models import Config, Image, OutImage, Experiment, Result, Stat
 # Register your database models here.
 
 admin.site.register(Config)
 admin.site.register(Image)
+admin.site.register(Experiment)
+admin.site.register(OutImage)
+admin.site.register(Result)
+admin.site.register(Stat)
