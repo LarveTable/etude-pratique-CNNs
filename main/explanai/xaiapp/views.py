a1ff8244dcd291119f70e85fb6424a0d2956e9c2
Sulaymane Dagnet <sulaymane.dagnet@insa-rennes.fr>
2024-06-04 15:26:43 +0200

    fixed result layout for better alignment

--- main/explanai/xaiapp/views.py
+++ main/explanai/xaiapp/views.py
@@ -14,18 +14,15 @@ from utils.load_coco_images import download
 
 # 
 # TODO :
+# - Use threading.Event() to create a flag to avoid duplicate threads: 
+#       run only if explanation.is_running = false
 # - Use pickle to save and get the model easily
 #       -> Pickle allows to serialize a class and compress it efficiently 
 #            into a binary
-# - Find a way to work with images maybe pickle again
 # - Use JSON for config files 
 # - Use CSRF_token for security in form  
 # - check streamlit for deployment
 # - check https://www.pythonanywhere.com/ for deployment
-# - create a requestExperiment class containing all the data necessary
-# - process_request method to call all algorithm and gives the output as a experiment output 
-# - sub_process to get image by images 
-# - use django forms
 
 # Homepage serving
 def home(request):
